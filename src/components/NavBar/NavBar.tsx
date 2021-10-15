811b6a429f45d52bd9eb7d30079db34f661429d4
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2021-10-15 02:06:37 -0400

    Fix image flicker problem
    Convert several of the SVGs in use into React components using svgr
    Revert LandingTabs back to using useState for now, update initial state
    only on browser (fixes hydration error). -- we should probably
    synchronize the session with the app state; at least some of it.
    
    Adds `loading="eager" to main landing images, this fixes flicker
    Add images as imports, which stops all of them from being loaded each
    request
    Adds suppressHydrationWarning to LandingTabs, since it relies on
    app state but must be rendered server-side

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,11 @@
+import { AdsSmallLogo } from '@components/images';
+import { isBrowser } from '@utils';
+import clsx from 'clsx';
 import Link from 'next/link';
 import React, { FC } from 'react';
-import { ThemeDropdown } from './ThemeDropdown';
 import styles from './NavBar.module.css';
-import clsx from 'clsx';
 import { NavMenus } from './NavMenus';
-import { isBrowser } from '@utils';
+import { ThemeDropdown } from './ThemeDropdown';
 
 export const NavBar: FC = () => {
   const navbarClasses = clsx(styles['navbar-bg-color'], 'relative flex items-center');
@@ -14,7 +15,7 @@ export const NavBar: FC = () => {
       <div className="px-4 sm:px-6 lg:px-8">
         <Link href="/">
           <a className="flex items-center h-12">
-            <img className="w-10 h-10" src="/img/transparent_logo.svg" aria-hidden="true" />
+            <AdsSmallLogo className="w-10 h-10" aria-hidden />
             <h1 className="hidden ml-2 text-white text-2xl font-bold sm:inline">SciX</h1>
           </a>
         </Link>
